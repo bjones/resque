wistia/resque-mongo

Worker#unregister_worker shouldn't call done_working
unregister_worker faulty increments Stat[:processed] by calling done_working

test/worker_test.rb
@@ -226,4 +226,11 @@ context "Resque::Worker" do
       assert_equal 1, Resque.workers.size
     end
   end
+
+  test "Pprocessed jobs count" do
+    @worker.work(0)
+    assert_equal 1, Resque.info[:processed]
+  end
+
+
 end

bjones/resque

test/worker_test.rb
@@ -226,4 +226,9 @@ context "Resque::Worker" do
       assert_equal 1, Resque.workers.size
     end
   end
+
+  test "Processed jobs count" do
+    @worker.work(0)
+    assert_equal 1, Resque.info[:processed]
+  end
 end