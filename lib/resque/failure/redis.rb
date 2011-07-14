bjones/resque

commit d1cfe4dd495be47f44d6d7ef090212eae854cbf3

Shorten the backtrace logged by Resque::Failure::Redis to only include the job by removing Resque and its callers (like Rake) from the backtrace.

lib/resque/failure/redis.rb
@@ -9,7 +9,7 @@ module Resque
           :payload   => payload,
           :exception => exception.class.to_s,
           :error     => exception.to_s,
-          :backtrace => Array(exception.backtrace),
+          :backtrace => filter_backtrace(Array(exception.backtrace)),
           :worker    => worker.to_s,
           :queue     => queue
         }
@@ -40,6 +40,10 @@ module Resque
         id = rand(0xffffff)
         Resque.redis.lset(:failed, index, id)
         Resque.redis.lrem(:failed, 1, id)
+      end
+
+      def filter_backtrace(backtrace)
+        backtrace.first(backtrace.index {|item| item.include?('/lib/resque/job.rb')})
       end
     end
   end

commit 73695e9165c0b7c6e8f4f46bb6c56805858d99c3

fix failure introduced by d1cfe4dd495be47f44d6d7ef090212eae854cbf3

lib/resque/failure/redis.rb
@@ -43,7 +43,8 @@ module Resque
       end
 
       def filter_backtrace(backtrace)
-        backtrace.first(backtrace.index {|item| item.include?('/lib/resque/job.rb')})
+        index = backtrace.index { |item| item.include?('/lib/resque/job.rb') }
+        backtrace.first(index.to_i)
       end
     end
   end